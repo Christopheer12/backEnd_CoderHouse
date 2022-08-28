1b1771fd64900e3f80c5667c7825db5a709e875c
cristian guerrero <cristian1guerrero@hotmail.com>
2022-08-28 13:30:44 -0300

    master

---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+class Contenedor{
+    constructor(name){
+        this.name =name;
+    }
+
+    save(informacion){
+        let id
+
+        return id
+    }
+    getById(id){
+
+    }
+    getAll(){
+        let objeto
+        return objeto
+    }
+    deleteById(id){
+
+    }
+    deleteAll(){
+
+    }
+}
+
