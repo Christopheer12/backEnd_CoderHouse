234cde6820ccb939246fed7b269b169c845af289
cristian guerrero <cristian1guerrero@hotmail.com>
2022-09-04 23:46:25 -0300

    nodemon

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+app.get('/',(req, res)=>{
+    res.send('Esto es elñ inicio de la pagina')
+})
+
 const connectedServer = app.listen(PORT, ()=>{
     console.log(`el servidor usa el puerto ${PORT}`)
 });
