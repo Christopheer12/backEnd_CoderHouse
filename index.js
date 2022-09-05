66cb78fae68ac8d3479fe9bcb2ca563436377d90
cristian guerrero <cristian1guerrero@hotmail.com>
2022-09-05 00:15:39 -0300

    glitch

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+// https://glitch.com/edit/#!/expresscoderhouse?path=server.js%3A21%3A2
+ 
 const express = require('express');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.get('/',(req, res)=>{
     res.send('Esto es el inicio de la pagina, para arancar nodemon se usar : npm rum start-dev ')
+})
+app.get('/login', (req,res)=>{
+    res.send('Hola. ¿Listo para ingresar?')
+})
+
+app.get('*',(req,res)=>{
+   res.status(404).send('<h1>ERROR 404</h1><h2>¡ESTO ES REAL HIJO! </h2>')
 })
 
 const connectedServer = app.listen(PORT, ()=>{
