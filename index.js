46c4590f978d09cbd04bb5cd61bef133e40b83dd
cristian guerrero <cristian1guerrero@hotmail.com>
2022-09-04 23:21:19 -0300

    desafio manejoDeArchivosEnJavascript

--- index.js
+++ index.js
@@ -1,17 +1,114 @@
-class Usuario{
-    constructor(nombre, apellido, libros, mascostas){
-        this.nombre = nombre//string
-        this.apellido = apellido,//string
-        this.libros =libros,//object
-        this.mascostas =mascostas//string
+const fs = require('fs')
+
+class Contenedor{
+    constructor(name){
+        this.name =name;
     }
 
+    async save(informacion){
+        
+        try{
+            let contenido = await fs.promises.readFile(`./${this.name}`,`utf-8`)
+            let contendijson =JSON.parse(contenido)
+            let ultimoIndice = contendijson.length -1
+            let ultimoId = contendijson[ultimoIndice].id
+            informacion.id = ultimoId + 1
+            let id = informacion.id
+            contendijson.push(informacion)
+            await fs.promises.writeFile(`./${this.name}`,JSON.stringify(contendijson) )
+            return id
+        }
+        catch(error){
+            console.log(error)
+        }
+
+       
+    }
+
+    async getById(id){
+        try{
+            let contenido = await fs.promises.readFile(`./${this.name}`, `utf-8`)
+            let contendijson = JSON.parse(contenido)
+            let contenidoExtraidoDelArray 
+            contendijson.forEach(element =>{
+                if(element.id == id){
+                    contenidoExtraidoDelArray = element
+                }
+            });
+            return contenidoExtraidoDelArray
+            
+        }
+        catch(error){
+            console-log("error getById")
+        }
+        
+
+    }
+    async getAll(){
+       
+        try{
+            let contenido = await fs.promises.readFile(`./${this.name}`, `utf-8`)
+            let contendijson = JSON.parse(contenido)
+            return contendijson
+        }
+        catch(error){
+            console.log("error getAll")
+        }
+    }
+    async deleteById(id){
+        try{
+            let contenido = await fs.promises.readFile(`./${this.name}`, `utf-8`)
+            let contendijson = JSON.parse(contenido)
+            let nuevoContendido = contendijson.filter(
+                (element => element.id !== id)
+            )
+            await fs.promises.writeFile(
+                `./${this.name}`,JSON.stringify(nuevoContendido)
+
+            )
+            
+        }
+        catch(error){
+            console.log("error delteById")
+        }
+        
+
+    }
+   async deleteAll(){
+    try{
+        await fs.promises.writeFile(`./${this.name}`,JSON.stringify([]))
+        
+    }
+    catch(error){
+        console.log("error deleteAll")
+    }
+    
+
+    }
 }
-const persona1 = new Usuario("Bojack","HorseMan",[{
-    libro:"Secretaria",
-    autor:"Diane Nguyen",
-},{
-    libro:"One Trick Pony",
-    autor:"Diane Nguyen"
-},],"Todd")
-console.log(persona1)
+
+let contenedor = new Contenedor("Productos.json")
+
+let informacionNueva ={
+    "title": "Tijera",
+    "price": 183.45,
+    "id":4
+}
+
+/* contenedor.save(informacionNueva).then(respuestaDePromesa=>{
+    console.log(respuestaDePromesa)
+}) */
+
+/* contenedor.getById(1).then(result=>{
+    console.log(result)
+}) */
+/* contenedor.getAll().then(result=>{
+    console.log(result)
+}) */
+
+/* contenedor.deleteById(2).then(result=>{
+    console.log(result)
+}) */
+contenedor.deleteAll().then(result=>{
+    console.log(result)
+})
