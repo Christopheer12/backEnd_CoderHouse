b9a2d8eee20e917467ac9cc2fbbe73e991842842
cristian guerrero <cristian1guerrero@hotmail.com>
2022-09-21 19:43:07 -0300

    .

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const path = require('path')
 const rutasApi = require('./routers/index')
 const express = require('express')
+const Productos = require('./model/productos')
 
 const app= express()
 const PORT = process.env.PORT || 8080
+const productos = new Productos()
+
 
 app.set ('views', './views')
 app.set('view engine', 'pug')
@@ -11,7 +14,7 @@ app.set('view engine', 'pug')
 app.use(express.static(path.resolve(__dirname,'./public')))
 
 app.get('/', (req,res)=>{
-  res.render('index')
+  res.render('index',{showList:true, productos:productos.getAll()})
 })
 
 /* app.user ('/api', rutasApi)
