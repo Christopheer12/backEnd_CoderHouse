3691ab30ea99d35ea67ffe1070bbb787fae4cea7
cristian guerrero <cristian1guerrero@hotmail.com>
2022-09-21 00:22:56 -0300

    2.0

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ const express = require('express');
  const fs = require('fs/promises');
  const path = require('path')
  const{engine}= require('express-handlebars')
-
+ const Productos = require('./model/productos')
  const PORT = 8080
 
  const app = express();
+ const productos = new Productos()
+
 
 
  app.engine('hbs', engine({
-
-
-
     extname: 'hbs',
     defaultLayout: 'main.hbs',
     layoutsDir: path.resolve(__dirname, './views/layouts'),
@@ -20,5 +19,8 @@ const express = require('express');
 
  app.set('views','./views')
  app.set ('view engine','hbs')
+ app.get('/', (req,res)=>{
+   res.render('index',{mostrarProductos:true, productos:productos.getAll()})
+ })
 
  app.listen(PORT,()=>console.log ('puesto listo en', PORT))
