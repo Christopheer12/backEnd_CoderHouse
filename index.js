16454dcffec3a51ed00532aa125989b46e622985
cristian guerrero <cristian1guerrero@hotmail.com>
2022-09-24 01:43:59 -0300

    .

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const rutasApi = require('./routers/index')
 const express = require('express')
 const Productos = require('./model/productos')
 
@@ -12,18 +11,36 @@ app.set ('views', './views')
 app.set('view engine', 'ejs')
 
 app.use(express.static(path.resolve(__dirname,'./public')))
-
+app.use(express.json())
+app.use(express.urlencoded({ extended:false}))
 /* app.get('/', (req,res)=>{
   res.render('index',{showList:true, productos:productos.getAll()})
 }) */
 
-app.get('/datos', (req,res)=>{
+/* app.get('/datos', (req,res)=>{
   res.render('main',req.query)
-})
+}) */
 
-app.get('/ejs',(req,res)=>{
+app.get('/',(req,res)=>{
   res.render('index',{listaDeProductos: false, productos:productos.getAll()})
 });
+
+app.post('/',(res,req)=>{
+  productos.save(req.body)
+  res.redirect('/')
+})
+
+
+
+
+
+
+
+
+
+
+
+
 /* app.user ('/api', rutasApi)
  */
 
