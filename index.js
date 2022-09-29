080877a732b2db32c54ce6be25d33d9f2dd412fa
cristian guerrero <cristian1guerrero@hotmail.com>
2022-09-29 15:00:33 -0300

    usuario conectado a servidor

--- index.js
+++ index.js
@@ -1,43 +1,19 @@
-const path = require('path')
 const express = require('express')
-const Productos = require('./model/productos')
+const{Server: HttpServer}= require('http')
+const{Server: SocketServer}= require('socket.io')
 
-const app= express()
-const PORT = process.env.PORT || 8080
-const productos = new Productos()
+const PORT = process.env.PORT || 8080;
+const app= express();
 
+app.use(express.static("./public"))
 
-app.set ('views', './views')
-app.set('view engine', 'ejs')
+const httpServer = new HttpServer(app)
 
-app.use(express.static(path.resolve(__dirname,'./public')))
-app.use(express.json())
-app.use(express.urlencoded({ extended:false}))
-/* app.get('/', (req,res)=>{
-  res.render('index',{showList:true, productos:productos.getAll()})
-}) */
-
-/* app.get('/datos', (req,res)=>{
-  res.render('main',req.query)
-}) */
-
-
-
-app.get('/',(req,res)=>{
-  res.render('index',{listaDeProductos: true, productos:productos.getAll()})
-});
-
-app.post('/',(req,res)=>{
-  console.log(req.body)
-  productos.save(req.body)
-  res.redirect('/')
+httpServer.listen(PORT,()=>{
+    console.log(`Puesto ${PORT} activo`)
 })
 
-
-const connectedSever = app.listen(PORT,()=>{
-  console.log (`servidorr activo en ${PORT}`)
-})
-
-connectedSever.on ('error', (error)=>{
-  console.log(error.message)
+const io = new SocketServer(httpServer)
+io.on('connection',()=>{
+    console.log("se conecto un usuario")
 })
---
+++ index.jsx
@@ -0,0 +1,43 @@
+const path = require('path')
+const express = require('express')
+const Productos = require('./model/productos')
+
+const app= express()
+const PORT = process.env.PORT || 8080
+const productos = new Productos()
+
+
+app.set ('views', './views')
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.resolve(__dirname,'./public')))
+app.use(express.json())
+app.use(express.urlencoded({ extended:false}))
+/* app.get('/', (req,res)=>{
+  res.render('index',{showList:true, productos:productos.getAll()})
+}) */
+
+/* app.get('/datos', (req,res)=>{
+  res.render('main',req.query)
+}) */
+
+
+
+app.get('/',(req,res)=>{
+  res.render('index',{listaDeProductos: true, productos:productos.getAll()})
+});
+
+app.post('/',(req,res)=>{
+  console.log(req.body)
+  productos.save(req.body)
+  res.redirect('/')
+})
+
+
+const connectedSever = app.listen(PORT,()=>{
+  console.log (`servidorr activo en ${PORT}`)
+})
+
+connectedSever.on ('error', (error)=>{
+  console.log(error.message)
+})
