7e6ffc164c7c9826f8bad66ab84c4e341c404196
cristian guerrero <cristian1guerrero@hotmail.com>
2022-10-03 23:14:06 -0300

    .

--- public/main.js
+++ public/main.js
@@ -1,8 +1,22 @@
 //@ts-nocheck
-const socket =io()
+const socket = io();
+
+const renderMessages = (data) => {
+  const html = data
+    .map((elem) => {
+      let fragment = `<div class="other-messages-container"><div class="other-messages"><span><b>${elem.username}</b> ${elem.time}</span><br /><span>${elem.text}</span></div></div>`;
+      return fragment;
+    })
+    .join("\n");
+  document.getElementById("messages").innerHTML = html;
+};
 
 //join chat
 
-const { username } = Qs.parse(window.location.search, {ignoreQueryPrefix: true})
+const { username } = Qs.parse(window.location.search, {
+  ignoreQueryPrefix: true,
+});
 
-socket.emit('join-chat',{username})
+socket.emit("join-chat", { username });
+
+socket.on("chat-message", (data) => {});
