4a72d95e5759ffd6b14e01d84bb29cbffd22ca71
cristian guerrero <cristian1guerrero@hotmail.com>
2022-10-06 09:23:44 -0300

    1.0

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const path = require('path')
 const express = require('express')
-const Productos = require('./model/productos')
+const{Server: HttpSever}= require('http')
+const{Server: SocketServer}= require('socket.io')
 
-const app= express()
+
 const PORT = process.env.PORT || 8080
+const app= express()
+const httpSever = new HttpSever(app)
+
+const Productos = require('./model/productos')
+const path = require('path')
 const productos = new Productos()
 
 
 app.set ('views', './views')
 app.set('view engine', 'ejs')
 
-app.use(express.static(path.resolve(__dirname,'./public')))
+/* app.use(express.static(path.resolve(__dirname,'./public'))) */
+app.use(express.static("./public"))
 app.use(express.json())
 app.use(express.urlencoded({ extended:false}))
-/* app.get('/', (req,res)=>{
-  res.render('index',{showList:true, productos:productos.getAll()})
-}) */
-
-/* app.get('/datos', (req,res)=>{
-  res.render('main',req.query)
-}) */
 
 
 
@@ -33,11 +32,28 @@ app.post('/',(req,res)=>{
   res.redirect('/')
 })
 
+const connectedServer = httpSever.listen(PORT,()=>{
+  console.log(`servidor activo en el puerto ${PORT}`)
+})
 
+
+const io = new SocketServer(httpSever)
+
+connectedServer.on('error',()=>{
+  console.log("errorrr pez")
+})
+
+
+
+io.on('connection',()=>{
+  console.log("se conecto nuevo usuario")
+})
+
+/* 
 const connectedSever = app.listen(PORT,()=>{
   console.log (`servidorr activo en ${PORT}`)
-})
+}) */
 
-connectedSever.on ('error', (error)=>{
+/* connectedSever.on ('error', (error)=>{
   console.log(error.message)
-})
+}) */
